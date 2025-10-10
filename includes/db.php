ff4936abe55a7801cedc6a8e207c11b4fb0bd64c
prassanna16 <forclassuse0607@gmail.com>
2025-10-11 01:25:08 +0530

    to check connection

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,14 @@
 <?php
-$host = "srv1951.hstgr.io";
+$host = "localhost";
 $dbname = "u994782675_portal";
 $username = "u994782675_Avis_portal";
 $password = "Avis@123456"; // Replace with actual password
 
 $conn = new mysqli($host, $username, $password, $dbname);
+
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+  die("❌ Connection failed: " . $conn->connect_error);
+} else {
+  echo "✅ Connected successfully to the database.";
 }
 ?>
